e1a73d2668d051797d78fb6bef6e5326360b8b95
OrenSource <orensource.com@gmail.com>
2019-03-17 19:18:45 +0100

    setup GroupName

---
+++ src/Domain/Model/Access/GroupName.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenDaje\IdentityAccess\Domain\Model\Access;
+
+final class GroupName
+{
+    private $name;
+
+    public function __construct(string $name)
+    {
+        if (strlen($name) === 0) {
+            throw new \InvalidArgumentException('Group name is required.');
+        }
+
+        if (strlen($name) < 5) {
+            throw new \InvalidArgumentException('Group name must be min. 5 characters.');
+        }
+
+        $this->name = $name;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function withName(string $name): GroupName
+    {
+        return new self($name);
+    }
+
+    public static function fromString(string $name): GroupName
+    {
+        return new self($name);
+    }
+
+    public function toString(): string
+    {
+        return $this->name;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    public function equals(GroupName $groupName): bool
+    {
+        return $this->name === $groupName->name;
+    }
+}
---
+++ tests/Domain/Model/Access/GroupNameTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenDaje\IdentityAccess\Tests\Domain\Model\Access;
+
+use OpenDaje\IdentityAccess\Domain\Model\Access\GroupName;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group unit
+ */
+class GroupNameTest extends TestCase
+{
+    private const FIRST_GROUP_NAME            = 'CUSTOMERS';
+    private const SECOND_GROUP_NAME           = 'SALES';
+    private const COPY_OF_FIRST_GROUP_NAME    = 'CUSTOMERS';
+
+    /** @test */
+    public function it_can_generate_GroupName_from_string()
+    {
+        $groupName = GroupName::fromString(self::FIRST_GROUP_NAME);
+
+        self::assertInstanceOf(GroupName::class, $groupName);
+        self::assertEquals(self::FIRST_GROUP_NAME, $groupName->name());
+        self::assertEquals(self::FIRST_GROUP_NAME, $groupName->toString());
+        self::assertEquals(self::FIRST_GROUP_NAME, $groupName->__toString());
+    }
+
+    /** @test */
+    public function it_returns_a_new_value_object_if_modified()
+    {
+        $groupName = GroupName::fromString(self::FIRST_GROUP_NAME);
+
+        $groupName = $groupName->withName(self::SECOND_GROUP_NAME);
+
+        self::assertInstanceOf(GroupName::class, $groupName);
+        self::assertEquals(self::SECOND_GROUP_NAME, $groupName->name());
+    }
+
+    /** @test */
+    public function it_can_be_compared()
+    {
+        $first = GroupName::fromString(self::FIRST_GROUP_NAME);
+        $second = GroupName::fromString(self::SECOND_GROUP_NAME);
+        $copyOfFirst = GroupName::fromString(self::COPY_OF_FIRST_GROUP_NAME);
+
+        self::assertFalse($first->equals($second));
+        self::assertTrue($first->equals($copyOfFirst));
+        self::assertFalse($second->equals($copyOfFirst));
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function empty_group_name_should_throw_exception()
+    {
+        GroupName::fromString('');
+    }
+
+    /**
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function short_group_name_should_throw_exception()
+    {
+        GroupName::fromString('acme');
+    }
+}
