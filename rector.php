363533de6095825c0581fce4d5883ce35bd3dbe5
zerai <teclaizerai@gmail.com>
2021-12-01 17:32:28 +0100

    [Tools] added base rector configuration (dead code).

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -11,8 +12,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
-        __DIR__ . '/src'
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
     ]);
+
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_72);
+
+    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan-for-config.neon');
+
 
     // Define what rule sets will be applied
     $containerConfigurator->import(SetList::DEAD_CODE);
